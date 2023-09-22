5e5f3327f707d567fdedc33e4c5e519c932f2082
Parth Passi <parthpassi@gmail.com>
2023-09-22 16:49:01 +0530

    Commit 2

--- app/src/main/java/com/example/raksha/Login.java
+++ app/src/main/java/com/example/raksha/Login.java
@@ -1,13 +1,39 @@
 package com.example.raksha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+    Button btn;
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn = findViewById(R.id.signup_button);
+        btn1=findViewById(R.id.login_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Signup.class);
+                startActivity(i);
+
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i1);
+            }
+        });
+
+
     }
 }
