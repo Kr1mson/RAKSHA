800df128f20158adeb02e8f991e8018b72f93865
Parth Passi <parthpassi@gmail.com>
2023-09-23 00:38:55 +0530

    Commit 5

--- app/src/main/java/com/example/raksha/Signup.java
+++ app/src/main/java/com/example/raksha/Signup.java
@@ -12,25 +12,35 @@ import android.widget.EditText;
 import com.example.raksha.databinding.ActivitySignupBinding;
 
 public class Signup extends AppCompatActivity {
-    Button btn;
+    public ActivitySignupBinding binding;
+    String full_name, email, password;
+    FirebaseDatabase userdb;
+    DatabaseReference reference;
+    Button sbmt_btn;
+
     EditText Name_edtxt,Email_edtxt,Pswd_edtxt,RePswd_edtxt;
     CheckBox chk1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        btn=findViewById(R.id.submit_btn);
+        sbmt_btn=findViewById(R.id.submit_btn);
         Name_edtxt=findViewById(R.id.name_edtxt);
         Email_edtxt=findViewById(R.id.email_edtxt);
         Pswd_edtxt=findViewById(R.id.Password);
         RePswd_edtxt=findViewById(R.id.RePassword);
         chk1=findViewById(R.id.chkbox);
-        btn.setOnClickListener(new View.OnClickListener() {
+        sbmt_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent i = new Intent(getApplicationContext(),Login.class);
-                startActivity(i);
+                full_name = Name_edtxt.getText().toString();
+                email = Email_edtxt.getText().toString();
+                password = Pswd_edtxt.getText().toString();
+                if(!full_name.isEmpty() && !email.isEmpty() && !password.isEmpty()){
+                    Users users = new Users(full_name, email, password);
+                }
             }
         });
 
---
+++ app/src/main/java/com/example/raksha/Users.java
@@ -0,0 +1,43 @@
+package com.example.raksha;
+
+public class Users {
+    String full_name, email, password;
+
+    public Users() {
+    }
+
+    public Users( String full_name, String email, String password) {
+
+        this.full_name = full_name;
+        this.email = email;
+        this.password = password;
+
+    }
+
+
+
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
