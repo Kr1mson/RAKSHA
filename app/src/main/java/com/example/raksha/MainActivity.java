3d3e2486dad8ee43ebba3aa2f9d6f6af631ea1a1
Parth Passi <parthpassi@gmail.com>
2023-09-23 04:39:49 +0530

    Commit 7

--- app/src/main/java/com/example/raksha/MainActivity.java
+++ app/src/main/java/com/example/raksha/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.raksha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import android.content.Intent;
-import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView
+        .OnNavigationItemSelectedListener{
     Button btn;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById(R.id.map_button);
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.Home);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +36,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    Map firstFragment = new Map();
+    SOS secondFragment = new SOS();
+    Settings thirdFragment = new Settings();
+
+    Home fourthFragment=new Home();
+
+    Alerts fifthFragment=new Alerts();
+    @Override
+    public boolean
+    onNavigationItemSelected(@NonNull MenuItem item)
+    {
+
+        switch (item.getItemId()) {
+            case R.id.Settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, thirdFragment).commit();
+                return true;
+
+            case R.id.maps:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
+                return true;
+
+            case R.id.SOS:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, secondFragment).commit();
+                return true;
+
+            case R.id.Home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fourthFragment).commit();
+                return true;
+
+            case R.id.Alerts:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fifthFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/raksha/Map.java
@@ -0,0 +1,24 @@
+package com.example.raksha;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Map extends Fragment {
+
+    public Map(){
+        // require a empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
