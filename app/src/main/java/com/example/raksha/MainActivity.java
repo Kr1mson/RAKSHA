46fed3b0fa25dc6e5d1caa800051ea3913984dcb
Parth Passi <parthpassi@gmail.com>
2023-09-22 19:06:20 +0530

    Commit 3

--- app/src/main/java/com/example/raksha/MainActivity.java
+++ app/src/main/java/com/example/raksha/MainActivity.java
@@ -8,12 +8,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn=findViewById(R.id.map_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
