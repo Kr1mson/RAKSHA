a929085e520ca3a8a9c13af6b54c6f771ecee54a
Parth Passi <parthpassi@gmail.com>
2023-09-24 13:34:51 +0530

    Commit 16

--- app/src/main/java/com/example/raksha/Agency_Signup.java
+++ app/src/main/java/com/example/raksha/Agency_Signup.java
@@ -1,20 +1,43 @@
 package com.example.raksha;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Agency_Signup extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     String[] types = { "Fire Department", "Emergency Medical Services ",
             "Search and Rescue", "Disaster Response Organization",
             "Law Enforcement", "Other" };
+    Button btLocation;
+    TextView tvLatitude, tvLongitude;
+    FusedLocationProviderClient client;
     Button btn1;
     EditText hidden1;
     EditText Agency_edtxt;
@@ -39,6 +62,7 @@ public class Agency_Signup extends AppCompatActivity implements AdapterView.OnIt
         ArrayAdapter ad = new ArrayAdapter(this, R.layout.spinner, types);
         ad.setDropDownViewResource(R.layout.spinner);
         spin.setAdapter(ad);
+        View view;
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,6 +75,41 @@ public class Agency_Signup extends AppCompatActivity implements AdapterView.OnIt
                 Type=a;
             }
         });
+        // Assign variable
+        btLocation = findViewById(R.id.bt_location);
+        tvLatitude = findViewById(R.id.tv_latitude);
+        tvLongitude =findViewById(R.id.tv_longitude);
+
+        // Initialize location client
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        btLocation.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override public void onClick(View view)
+                    {
+                        // check condition
+                        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                            // When permission is granted
+                            // Call method
+                            getCurrentLocation();
+                        }
+                        else {
+                            // When permission is not granted
+                            // Call method
+                            requestPermissions(
+                                    new String[] {
+                                            android.Manifest.permission
+                                                    .ACCESS_FINE_LOCATION,
+                                            Manifest.permission
+                                                    .ACCESS_COARSE_LOCATION },
+                                    100);
+                        }
+                    }
+                });
+
+        // Return view
+
+
     }
     @Override
     public void onItemSelected(AdapterView arg0, View arg1, int position, long id)
@@ -68,6 +127,132 @@ public class Agency_Signup extends AppCompatActivity implements AdapterView.OnIt
     public void onNothingSelected(AdapterView arg0)
     {
         // Auto-generated method stub
+    }
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+        // Check condition
+        if (requestCode == 100 && (grantResults.length > 0)
+                && (grantResults[0] + grantResults[1]
+                == PackageManager.PERMISSION_GRANTED)) {
+            // When permission are granted
+            // Call method
+            getCurrentLocation();
+        }
+        else {
+            // When permission are denied
+            // Display toast
+            Toast
+                    .makeText(this,
+                            "Permission denied",
+                            Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation()
+    {
+        // Initialize Location manager
+        LocationManager locationManager
+                = (LocationManager)this
+                .getSystemService(
+                        Context.LOCATION_SERVICE);
+        // Check condition
+        if (locationManager.isProviderEnabled(
+                LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER)) {
+            // When location service is enabled
+            // Get last location
+            client.getLastLocation().addOnCompleteListener(
+                    new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(
+                                @NonNull Task<Location> task)
+                        {
+
+                            // Initialize location
+                            Location location
+                                    = task.getResult();
+                            // Check condition
+                            if (location != null) {
+                                // When location result is not
+                                // null set latitude
+                                tvLatitude.setText(
+                                        String.valueOf(
+                                                location
+                                                        .getLatitude()));
+                                // set longitude
+                                tvLongitude.setText(
+                                        String.valueOf(
+                                                location
+                                                        .getLongitude()));
+                            }
+                            else {
+                                // When location result is null
+                                // initialize location request
+                                LocationRequest locationRequest
+                                        = new LocationRequest()
+                                        .setPriority(
+                                                LocationRequest
+                                                        .PRIORITY_HIGH_ACCURACY)
+                                        .setInterval(10000)
+                                        .setFastestInterval(
+                                                1000)
+                                        .setNumUpdates(1);
+
+                                // Initialize location call back
+                                LocationCallback
+                                        locationCallback
+                                        = new LocationCallback() {
+                                    @Override
+                                    public void
+                                    onLocationResult(
+                                            LocationResult
+                                                    locationResult)
+                                    {
+                                        // Initialize
+                                        // location
+                                        Location location1
+                                                = locationResult
+                                                .getLastLocation();
+                                        // Set latitude
+                                        tvLatitude.setText(
+                                                String.valueOf(
+                                                        location1
+                                                                .getLatitude()));
+                                        // Set longitude
+                                        tvLongitude.setText(
+                                                String.valueOf(
+                                                        location1
+                                                                .getLongitude()));
+                                    }
+                                };
+
+                                // Request location updates
+                                client.requestLocationUpdates(
+                                        locationRequest,
+                                        locationCallback,
+                                        Looper.myLooper());
+                            }
+                        }
+                    });
+        }
+        else {
+            // When location service is not enabled
+            // open location setting
+            startActivity(
+                    new Intent(
+                            Settings
+                                    .ACTION_LOCATION_SOURCE_SETTINGS)
+                            .setFlags(
+                                    Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
     }
 
 }
