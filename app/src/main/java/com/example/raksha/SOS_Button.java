29486540bd1c0a56da21d29c6cd8843b403ded2c
Parth Passi <parthpassi@gmail.com>
2023-10-20 15:05:53 +0530

    commit_parth

--- app/src/main/java/com/example/raksha/SOS_Button.java
+++ app/src/main/java/com/example/raksha/SOS_Button.java
@@ -1,15 +1,38 @@
 package com.example.raksha;
 
+import static android.Manifest.permission.CALL_PHONE;
+
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.widget.RemoteViews;
+
+import androidx.core.content.ContextCompat;
 
 
 public class SOS_Button extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
+        CharSequence Number = SOS_ButtonConfigureActivity.loadTitlePref(context, appWidgetId);
+        Intent i = new Intent(Intent.ACTION_CALL);
+        i.setData(Uri.parse("tel:"+Number));
+        if (ContextCompat.checkSelfPermission(context, CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_IMMUTABLE);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.s_o_s__button);
+            views.setOnClickPendingIntent(R.id.widget_sos, configPendingIntent);
+            ComponentName componentName = new ComponentName(context.getPackageName(), AppWidgetProvider.class.getName());
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+
+
+        }
 
         CharSequence widgetText = SOS_ButtonConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
@@ -17,7 +40,7 @@ public class SOS_Button extends AppWidgetProvider {
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+
     }
 
     @Override
@@ -38,7 +61,7 @@ public class SOS_Button extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+
     }
 
     @Override
--- app/src/main/java/com/example/raksha/SOS_ButtonConfigureActivity.java
+++ app/src/main/java/com/example/raksha/SOS_ButtonConfigureActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.raksha.databinding.SOSButtonConfigureBinding;
@@ -18,6 +19,7 @@ public class SOS_ButtonConfigureActivity extends Activity {
 
     private static final String PREFS_NAME = "com.example.raksha.SOS_Button";
     private static final String PREF_PREFIX_KEY = "appwidget_";
+
     int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
     EditText mAppWidgetText;
     View.OnClickListener mOnClickListener = new View.OnClickListener() {
@@ -25,6 +27,7 @@ public class SOS_ButtonConfigureActivity extends Activity {
             final Context context = SOS_ButtonConfigureActivity.this;
 
             // When the button is clicked, store the string locally
+
             String widgetText = mAppWidgetText.getText().toString();
             saveTitlePref(context, mAppWidgetId, widgetText);
 
